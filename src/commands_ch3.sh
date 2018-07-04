c0c0449f95456bf58f3210759df1dbc126418cc1
vityabool <vtsykunov@hotmail.com>
2018-07-04 15:46:47 +0300

    fixes

--- src/commands_ch3.sh
+++ src/commands_ch3.sh
@@ -6,21 +6,26 @@ az provider register -n Microsoft.Compute
 az provider register -n Microsoft.ContainerService
 
 # Crate AKS cluster 
-az group create --name LinuxConfEastUS --location eastus
-az aks create --resource-group LinuxConfEastUS --name MinecraftClass --node-count 1 --generate-ssh-keys
+az group create --name AKSDemoA --location westeurope
+az aks create --resource-group AKSDemoA --name MinecraftAKK --node-count 1 --generate-ssh-keys
 
-# Install CLI extentions
+# Install CLI extensions
 az aks install-cli
 
 # Login to the cluster
-az aks get-credentials --resource-group LinuxConfEastUS --name MinecraftClass
+az aks get-credentials --resource-group AKSDemoA --name MinecraftAKK
 
-# Show availible nodes
+# Show available nodes
 kubectl get nodes
 
 # Start deployment
 kubectl create -f deployment.yaml
+
 # Check deployment status
 kubectl get deployments
 kubectl get pods
 kubectl get services
+
+# Delete cluster after
+az aks delete --resource-group AKSDemoA --name MinecraftAKK
+az group delete AKSDemoA
